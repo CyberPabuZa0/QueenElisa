b003cd0b4cf251d7030b8194ca61717421ad94f2
Pabasara Malinda <142970254+CyberPabuZa0@users.noreply.github.com>
2024-10-20 17:41:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,8 +19,8 @@ module.exports = {
 
 Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   ALIVE_MEDIA: 'https://telegra.ph/file/d842677f522b6bef4dfe6.jpg',
-  MONGODB_URL: '', 
-  PREFIX: '.$&',
+  MONGODB_URL: 'mongodb+srv://pabasara:pabasara@123 @cluster0.om16l.mongodb.net/', 
+  PREFIX: '.',
   LANGUAGE: 'EN',
   PORT: 3000,
   BOT_NAME: '𝐐𝐔𝐄𝐄𝐍 𝐄𝐋𝐈𝐒𝐀',
